e091e97751117dc657e308743ae54cf91db8c64a
Phpar-Ph <146304604+Phpar-Ph@users.noreply.github.com>
2025-04-20 09:27:52 +0800

    add front end url

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://user-authentication-frontend-6460.onrender.com",
+];
 app.use(express.json());
 app.use(cookieParser());
 
